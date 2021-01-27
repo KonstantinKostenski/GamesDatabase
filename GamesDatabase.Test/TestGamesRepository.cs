583568e872970c4abc3e70383a25bc2788681f71
Konstantin.Kostenski <Konstantin_Kostenski@yahoo.com>
2021-01-27 12:17:05 +0200

    Added a test project.

---
+++ GamesDatabase.Test/TestGamesRepository.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using GamesDatabaseBusinessLogic.Interfaces;
+
+namespace GamesDatabase.Test
+{
+    public class Tests
+    {
+        public Tests()
+        {
+            IGameRepository gameRepository = 
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
